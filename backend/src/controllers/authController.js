bc19b570e7202ff73af2bec5ed0a31ebf20ef7ba
Anshu2607 <anshu260304@gmail.com>
2026-05-23 00:27:05 +0530

    Add JWT middleware and protected routes

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,115 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const prisma = require("../config/prisma");
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user,
+    });
+  } catch (error) {
+  console.log(error);
+
+  res.status(500).json({
+    error: error.message,
+    fullError: error,
+  });
+}
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+  console.log(error);
+
+  res.status(500).json({
+    error: error.message,
+    fullError: error,
+  });
+}
+};
+
+const getMe = async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+    });
+
+    res.status(200).json({
+      user,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+      fullError: error,
+    });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+  getMe,
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -12,7 +16,9 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = 5000;
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
